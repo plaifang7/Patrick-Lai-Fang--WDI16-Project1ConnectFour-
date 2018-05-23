e2ca79e35733a4e59382e40e3222c13cd0780f40
plaifang7 <plaifang7@gmail.com>
2018-05-23 06:02:18 -0400

    woke up at 4am because my mind was racing about project. I put some stuff together to pick jamies brain about. how to attach array to game-board? and move forward from there

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 $(document).ready(function () {
+    var player = 'red'
 
     // alert("Welcome to Connect 4! Choose a Game Mode.")
     function startGame() {
-        const gameBoard = [
+        var gameBoard = [
             [0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0],
@@ -10,9 +11,36 @@ $(document).ready(function () {
             [0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0]
         ]
+        for(let i =0; i <= 6; i++){
+            for(j = 0; j <=7; j++){
 
+            }
+        }
+
+        //or
+
+        var gameBoard = document.getElementsByClassName('game-board')
+
+        $('.grid')
+        // code to push a 1 or 2 into array if player takes their turn
+        // for (let x = 0; x < gameboard.length; x++) {
+        //     if (player % 2 === 1) {
+        //         gameBoard[x].push(1)
+
+        //     }
+        //     else if (player % 2 === 0) {
+        //         gameBoard[x].push(2)
         // console.log(gameBoard);
 
+    }
+    // I need to tie my gameBoard array to the information below and am having troubles with it.
+
+    // player turn
+    if (player == 'yellow') {
+        player = 'red'
+    }
+    else {
+        player = 'yellow'
     }
 
     //This will carry all functions for when the user hovers over the board and clicks a column.
@@ -20,52 +48,55 @@ $(document).ready(function () {
         //checks for an empty  slot on the grid
         function checkForEmptySlot(col) {
             const slots = $(`.gridrow[x = '${col}']`)
-            for(let i = slots.length- 1; i>= 0; i--){
-                const $slots = $(slots[i])
-                if(slots.hasClass('none')){
-
+            for (let i = slots.length - 1; i >= 0; i--) {
+                $slots = $(slots[i])
+                if (slots.hasClass('none')) {
+                    return $slots
                 }
-            }
+            
+            return null;
             console.log(slots)
+            }
         }
 
-        // this was supposed to create a hover effect
+        // this was supposed to create a hover effect, but MVP. save for later time
 
-        // $(' gridrow.none').on('mouseenter', function () {
-        //     const col = $(this).attr('x')
-        //     const $checkEmpty = checkForEmptySlot(col)
-        //     $checkEmpty.addClass('move-red')
-            // console.log(col);
+        $(' gridrow.none').on('mouseenter', function () {
+            const col = $(this).attr('x')
+            var $checkEmpty = checkForEmptySlot(col)
+            $checkEmpty.addClass('move-red')
+            console.log(col);
 
-        // })
-    
-        //click function for player to drop checker
-    $('.gridrow').click(function () {
-       const col = $(this).data('col')
-       const $checkEmpty = checkForEmptySlot(col)
-       $checkEmpty.removeClass('none')
-       $checkEmpty.addClass('p1')
-       
+        })
 
-        // console.log($(this).attr('x'))
+        //click function for player to drop checker. declared that there is an empty spot via check and then add checker in spot via removeCLass and addClass
+        $('.gridrow').click(function () {
+            const col = $(this).attr('col')
+            var $checkEmpty = checkForEmptySlot(col)
+            $checkEmpty.removeClass('none')
+            $checkEmpty.addClass('p1')
+            
+
+            console.log($(this).attr('x'))
 
 
 
-    })
+        })
 
-}
-clickFunct();
+    }
+
+    clickFunct();
 
 
 
 
 
     // for (let x = 0; x < gameboard.length; x++) {
-    //     if (playCheck % 2 === 1) {
+    //     if (player % 2 === 1) {
     //         gameBoard[x].push(1)
 
     //     }
-    //     else if (playCheck % 2 === 0) {
+    //     else if (player % 2 === 0) {
     //         gameBoard[x].push(2)
 
 
